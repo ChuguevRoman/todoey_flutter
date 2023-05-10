ed60e742d9b4dab5e6aeb40eaaec93c678e8cbdf
ChuguevRoman <5tiger5xman@gmail.com>
2023-05-10 14:21:56 +0300

    Checkbox responds to clicks. The text of the task also changes when the checkbox is pressed to crossed out.

--- lib/widgets/task_tile.dart
+++ lib/widgets/task_tile.dart
@@ -1,18 +1,43 @@
 import 'package:flutter/material.dart';
 
-class TaskTile extends StatelessWidget {
-  const TaskTile({
-    super.key,
-  });
+class TaskTile extends StatefulWidget {
+
+  @override
+  State<TaskTile> createState() => _TaskTileState();
+}
+
+class _TaskTileState extends State<TaskTile> {
+  bool isChecked = false;
+
 
   @override
   Widget build(BuildContext context) {
     return ListTile(
-      title: Text('This is a task'),
-      trailing: Checkbox(
-        value: false,
-        onChanged: (bool? newValue) {},
-      ),
+      title: Text('This is a task', style: TextStyle(
+        decoration: isChecked ? TextDecoration.lineThrough : null,
+      ),),
+      trailing: TaskCheckbox(checkboxState: isChecked,
+      toggleCheckboxState: (newValue) {
+        setState(() {
+          isChecked = newValue;
+        });
+      }),
+    );
+  }
+}
+
+class TaskCheckbox extends StatelessWidget {
+ final bool checkboxState;
+ final Function toggleCheckboxState;
+
+ TaskCheckbox({required this.checkboxState, required this.toggleCheckboxState});
+
+  @override
+  Widget build(BuildContext context) {
+    return Checkbox(
+      value: checkboxState,
+      activeColor: Colors.lightBlueAccent,
+      onChanged: (newValue) => toggleCheckboxState(newValue),
     );
   }
 }
